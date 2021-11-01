dadca7e26f7fee78073489e8fbf6f3fd1ddc5d0b
日高岳大 <takehiro.hidaka@persol.co.jp>
2021-11-01 15:48:58 +0900

    微修正

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,6 @@ echo $CURRENT
 cd ../
 
 # win用にbuild
-GOOS=windows GOARCH=amd64 go build -o dist/oic.exe ./src/main.go
+GOOS=windows GOARCH=amd64 go build -o dist/ags.exe ./src/main.go
 # mac用にbuild
-go build -o dist/oic ./src/main.go
+go build -o dist/ags ./src/main.go
